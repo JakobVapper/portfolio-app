4f945fd63750bc3aa68a3de16ced52582186f2f4
JakobVapper <jakob.vapper@ametikool.ee>
2023-11-07 17:39:48 +0200

    Finished the header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Logo from '../media/logo.png'
 import Burger from '../media/Burger.png'
+import Cross from '../media/Cross.png'
 
 const Navbar = () => {
+    const [nav,setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
+
     return (
         <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-black'>
             <div>
                 <img src={Logo} alt="Website logo" style={{width: '50px'}} />
             </div>
-            <div>
-                <ul className='flex'>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Experience</li>
-                    <li>Work</li>
-                    <li>Contact</li>
-                </ul>
+
+            <ul className='hidden md:flex'>
+                <li className='text-white'>Home</li>
+                <li className='text-white'>About</li>
+                <li className='text-white'>Experience</li>
+                <li className='text-white'>Work</li>
+                <li className='text-white'>Contact</li>
+            </ul>
+
+            <div onClick={handleClick} className='md:hidden z-10'>
+                {!nav ? <img style={{width: '50px'}} src={Burger} alt="Burger" /> : <img style={{width: '50px'}} src={Cross} alt="Cross" />}
             </div>
-            <div className='hidden'>
-                <img src={Burger} alt="Hamburger menu" style={{width: '50px'}} />
-            </div>
-            <ul className='hidden'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Experience</li>
-                <li>Work</li>
-                <li>Contact</li>
+
+            <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-black flex flex-col justify-center items-center'}>
+                <li className='py-6 text-4xl text-white'>Home</li>
+                <li className='py-6 text-4xl text-white'>About</li>
+                <li className='py-6 text-4xl text-white'>Experience</li>
+                <li className='py-6 text-4xl text-white'>Work</li>
+                <li className='py-6 text-4xl text-white'>Contact</li>
             </ul>
         </div>
     )
