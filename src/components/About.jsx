38abff4e558f753580f71e68dc093c0ae1a1817d
JakobVapper <jakob.vapper@ametikool.ee>
2023-11-09 20:43:08 +0200

    Edited mobile view

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -35,11 +35,11 @@ const About = () => {
     }, []);
 
     return (
-        <div className='bg-black flex lg:flex-row'>
+        <div className='bg-black lg:flex lg:flex-row md:flex-col'>
             <Lottie className='h-[600px]' animationData={Animation} />
             <motion.div initial="initial" animate={isVisible ? "animate" : "initial"} variants={textVariants} className='flex flex-col lg:max-w-[800px] place-items-center text-center'>
                 <motion.h1 variants={textVariants} className='bg-gradient-to-r from-violet-900 to-indigo-400 bg-clip-text text-transparent text-7xl font-bold pt-14'>About Me</motion.h1>
-                <motion.p variants={textVariants} className='text-indigo-300 w-[800px] pt-14'>Hi, I am Jakob! I am currently enrolled in the Web Development program at Kuressaare Ametikool, currently in my second year of studies. This program has provided me with a solid foundation in front-end and back-end technologies, including various frameworks and libraries. Looking ahead, I am eager to continue my academic journey and explore advanced topics in web development. I am also excited about the prospect of internships and professional experiences that will allow me to further refine my skills and make meaningful contributions to real-world projects.</motion.p>
+                <motion.p variants={textVariants} className='text-indigo-300 lg:w-[800px] pt-14'>Hi, I am Jakob! I am currently enrolled in the Web Development program at Kuressaare Ametikool, currently in my second year of studies. This program has provided me with a solid foundation in front-end and back-end technologies, including various frameworks and libraries. Looking ahead, I am eager to continue my academic journey and explore advanced topics in web development. I am also excited about the prospect of internships and professional experiences that will allow me to further refine my skills and make meaningful contributions to real-world projects.</motion.p>
             </motion.div>
         </div>
     )
